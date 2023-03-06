bd79b4062c3bafead9a932e47c0f268a8cf45495
sajibsaifullah <dusajibphy@gmail.com>
2023-03-06 17:34:02 +0600

    first commit

---
+++ interview-question.js
@@ -0,0 +1,25 @@
+/* 
+1. What is syntax error?
+2. What type of error you see? How will you fix it?
+            function error(a){
+                if(a>5){
+                    return true
+                }else{
+                    return false
+                
+
+            console.log(error(5))
+3. When do we get reference error?
+4. What type of an error will this code generate? How will you fix it?
+            function text(){
+                var str = "hello World";
+                return str;
+            }
+            console.log(str);
+5. a) What is wrong with the code below:
+            var foo;
+            console.log(foo.bar);
+    b) What type of error will the code generate?
+    c) What is the correct way to generate the code?
+
+*/
